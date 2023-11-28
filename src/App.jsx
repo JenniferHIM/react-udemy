826e31462a60bdfbc81491c76d3f82b9aab9fca2
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2023-11-28 12:07:42 +0100

    add base files + Header

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {/* <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            /> */}
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
