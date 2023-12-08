0442ce20d323ccc2fdbdf5fe15d9ac0860d6072b
Evgeniya Obernikhina <jenniferhim1201@gmail.com>
2023-12-08 20:24:45 +0100

    split up code to components

---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,24 @@
+import { CORE_CONCEPTS } from "../data";
+import CoreConcept from "./CoreConcept";
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concept</h2>
+      <ul>
+        {CORE_CONCEPTS.map((conceptItem) => (
+          <CoreConcept key={conceptItem.title} {...conceptItem} />
+        ))}
+        {/* <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
+        {/* <CoreConcept
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image}
+            /> */}
+      </ul>
+    </section>
+  );
+}
